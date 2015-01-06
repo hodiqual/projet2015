f53a70c46d61ff8d9ef5a577a1aa924824f27620
hodiqual <alexis.hodiquet@gmail.com>
2015-01-06 21:48:13 +0100

    Mettre le look and feel nimbus

--- src/main/java/fr/iessa/app/Application.java
+++ src/main/java/fr/iessa/app/Application.java
@@ -1,9 +1,34 @@
 package fr.iessa.app;
 
+import java.awt.Dimension;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.UIManager;
+import javax.swing.UIManager.*;
+
 public class Application {
 
 	public static void main(String[] args) {
-		System.out.println("AEROPORT");
+		
+		try {
+		    for (LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
+		        if ("Nimbus".equals(info.getName())) {
+		            UIManager.setLookAndFeel(info.getClassName());
+		            break;
+		        }
+		    }
+		} catch (Exception e) {
+		    //TODO Log4j -- Nimbus pas possible
+			System.out.println("AEROPORT");
+		}
+		
+		JFrame frame = new JFrame("A REFAIRE");
+	    //frame.setMinimumSize(new Dimension(800, 600));
+	    frame.add(new JButton("PROUT"));
+	    frame.add(new JButton("PROUT"));
+	    frame.validate();
+	    frame.setVisible(true);
 	}
 
 }
