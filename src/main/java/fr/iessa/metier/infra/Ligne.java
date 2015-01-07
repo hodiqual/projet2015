797cd96a452c4a0a0fcbb3fcd1801318fed8a9a1
hodiqual <alexis.hodiquet@gmail.com>
2015-01-07 15:06:48 +0100

    Ajout classe Ligne

---
+++ src/main/java/fr/iessa/metier/infra/Ligne.java
@@ -0,0 +1,15 @@
+package fr.iessa.metier.infra;
+
+import java.awt.geom.GeneralPath;
+
+/**
+ * Classe mère decrivant les lignes brisées
+ * @author hodiqual
+ *
+ */
+public class Ligne {
+	
+	/** La ligne brisée construite à partir des coordonnees */
+	GeneralPath _lignePointAPoint;
+
+}
