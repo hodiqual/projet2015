acdb47d9ea83cae806f5169154ff1656a2e4cb0d
duvernal <alexis.duv@gmail.com>
2015-01-12 18:11:14 +0100

    Modification Runway et création RunwayDAO

--- src/main/java/fr/iessa/dao/infra/RunwayDAO.java
+++ src/main/java/fr/iessa/dao/infra/RunwayDAO.java
@@ -3,11 +3,18 @@
  */
 package fr.iessa.dao.infra;
 
+import java.awt.Point;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
 import fr.iessa.dao.core.DAO;
+import fr.iessa.metier.infra.QFU;
 import fr.iessa.metier.infra.Runway;
 
+
 /**
- * @author duvernoy
+ * @author duvernal
  *
  */
 public class RunwayDAO implements DAO<Runway> {
@@ -16,10 +23,37 @@ public class RunwayDAO implements DAO<Runway> {
 	 * @see fr.iessa.dao.core.DAO#charger(java.lang.String)
 	 */
 	@Override
+	/**
+	 * Charge en memoire une ligne de texte decrivant une ligne 
+	 * ligneAtraiter: decrit un runway de la maniere suivante
+	 * "R 09L-27R 09L 27R -1682,1660;1009,1888 Z1;Z2;Z3;Z4;Z5;Z6;Z7;Z8"
+	 */
+
 	public Runway charger(String ligneAtraiter) {
-		// TODO Auto-generated method stub
-		return null;
+		
+		int i=0;
+		String nom, qfu1,qfu2;
+		java.awt.Point[] extremite = null;
+		String[] points = null;
+		Scanner scan = new Scanner(ligneAtraiter);
+		scan.useDelimiter(" |;|\n");
+		scan.next(); //skip R
+		nom = scan.next();
+		qfu1 = scan.next();
+		qfu2 = scan.next();
+		scan.useDelimiter(";|\n");
+		extremite[0] = new Point(scan.nextInt(), scan.nextInt());
+		extremite[1] = new Point(scan.nextInt(), scan.nextInt());
+		
+		while(scan.hasNext())
+		{
+			points[i] = scan.next();
+			i++;
+		}
+		
+		Runway r =  new Runway(nom, qfu1, qfu2,	extremite, points);
+		scan.close();
+		return r;
 	}
-	
 	
 }
--- src/main/java/fr/iessa/metier/infra/Runway.java
+++ src/main/java/fr/iessa/metier/infra/Runway.java
@@ -3,8 +3,11 @@
  */
 package fr.iessa.metier.infra;
 
-import java.util.ArrayList;
-import java.awt.Point;
+import java.util.LinkedList;
+import java.util.Queue;
+
+import fr.iessa.metier.type.TypeQFU;
+
 
 /**
  * Classe mère décrivant les pistes de la plateforme.
@@ -14,16 +17,72 @@ import java.awt.Point;
 public class Runway {
 
 	/** Le nom de la piste */
-	protected String _nom;
+	private String _nom;
 	
 	/** Le nom de la piste */
-	protected QFU _qfuL;
+	private QFU _qfuL;
 	
 	/** Le nom de la piste */
-	protected QFU _qfuR;
+	private QFU _qfuR;
 	
-	protected Point[] _extremite;
+	/** Les extremites de la piste */
+	private java.awt.Point[] _extremite;
 	
-	ArrayList<Point> _listepoints = new ArrayList();
+	/** La liste des points qui composent la piste */
+	private String[] _listepoints;
+	
+	
+	// Accessors
+	
+	
+	public Runway(String nom, String qfu1, String qfu2,
+			java.awt.Point[] extremite, String[] points) {
+
+		_nom=nom;
+		_qfuR.setType(TypeQFU.R);
+		_qfuL.setType(TypeQFU.L);
+		if (qfu1.substring(2,3) == "R")
+		{
+		_qfuR.setDegre(qfu1.substring(0,2));
+		_qfuL.setDegre(qfu2.substring(0,2));
+		
+		}
+		else
+		{
+		_qfuL.setDegre(qfu1.substring(0,2));
+		_qfuR.setDegre(qfu2.substring(0,2));
+		}
+		
+		_extremite=extremite;
+		_listepoints=points;
+	}
+
+	/**
+	 * @return the _nom
+	 */
+	public String get_nom() {
+		return _nom;
+	}
+
+	/**
+	 * @return the _QFUL
+	 */
+	public QFU get_QFUL() {
+		return _qfuL;
+	}
+	
+	/**
+	 * @return the _QFUR
+	 */
+	public QFU get_QFUR() {
+		return _qfuR;
+	}
+	
+	/**
+	 * @return the _extremite
+	 */
+	public Point[] get_extremite() {
+		return _extremite;
+	}
 	
 }
