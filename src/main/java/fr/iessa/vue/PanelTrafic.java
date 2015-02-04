17304ecdb4d4d76fc6325fdf78e1148c51bb12c8
hodiqual <alexis.hodiquet@gmail.com>
2015-02-04 21:48:13 +0100

    Optimization affichage carte

--- src/main/java/fr/iessa/vue/PanelTrafic.java
+++ src/main/java/fr/iessa/vue/PanelTrafic.java
@@ -3,6 +3,7 @@ package fr.iessa.vue;
 import java.awt.Dimension;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Observable;
@@ -15,6 +16,7 @@ import javax.swing.JPanel;
 import javax.swing.SwingWorker;
 
 import fr.iessa.controleur.Controleur;
+import fr.iessa.controleur.LibereMemoire;
 import fr.iessa.controleur.ModeleEvent;
 import fr.iessa.metier.Instant;
 import fr.iessa.metier.trafic.Trafic;
@@ -23,16 +25,18 @@ import fr.iessa.vue.trafic.ComponentVol;
 
 public class PanelTrafic extends JPanel implements PropertyChangeListener, Observer{
 
-	private Echelle _echelle;
+	private final Echelle _echelle;
+	private final Controleur _controleur;
 
     public PanelTrafic(Controleur controleur, Echelle echelle) {
         setOpaque(false);
+        
+        _controleur = controleur;
 
-		final ModeleEvent[] evts = { ModeleEvent.CHARGEMENT_TRAFIC_FICHIER_DONE,
-									 ModeleEvent.UPDATE_INSTANT };
-		controleur.ajoutVue(this,  evts) ;
+		final ModeleEvent[] evts = { ModeleEvent.CHARGEMENT_TRAFIC_FICHIER_DONE };
+		_controleur.ajoutVue(this,  evts) ;
 		
-		echelle.addObserver(this);
+
 		_echelle = echelle;
     }
 
@@ -46,7 +50,14 @@ public class PanelTrafic extends JPanel implements PropertyChangeListener, Obser
 	private final class InitializeComponentVols extends SwingWorker<Void,Void> {
 		@Override
 		protected Void doInBackground() throws Exception {
+			
 			_volsADessiner = _trafic.getVols().stream().collect(Collectors.toMap(Function.identity(), v -> new ComponentVol(v,_echelle)));
+
+			final ModeleEvent[] evts = { ModeleEvent.UPDATE_INSTANT };
+			_controleur.ajoutVue(PanelTrafic.this,  evts) ;
+			
+			_echelle.addObserver(PanelTrafic.this);
+			
 			return null;
 		}		
 	}
@@ -69,33 +80,17 @@ public class PanelTrafic extends JPanel implements PropertyChangeListener, Obser
 		}
 		
 	}
-	
-
-	
-	private final class UpdateComponentVols extends SwingWorker<Void,Void> {
-		@Override
-		protected Void doInBackground() throws Exception {
-			//_volsADessiner = _trafic.getVols().stream().collect(Collectors.toMap(Function.identity(), v -> new ComponentVol(v,_echelle)));
-			return null;
-		}		
-	}
-	
-	
-	private Set<ComponentVol> _volsCourant = new HashSet<ComponentVol>();
 
 	@Override
 	public void update(Observable o, Object arg) {
-		_volsADessiner.values().forEach(cv -> cv.update(this) );
+			_volsADessiner.values().forEach(cv -> cv.update(this) );
 	}  
 	
 	private void update(Instant instant) {
-		//_volsCourant.forEach(v -> remove(v));
-		//_volsCourant.clear();
-		//_trafic.getVols(instant).stream().forEach( v -> _volsCourant.add(_volsADessiner.get(v)) );
 		_volsADessiner.values().forEach(cv -> cv.update(this) );
-		//_volsCourant.forEach(v -> add(v));
 		revalidate();
 		repaint();
+		LibereMemoire.controleMemoire();
 	}  
 
 }
