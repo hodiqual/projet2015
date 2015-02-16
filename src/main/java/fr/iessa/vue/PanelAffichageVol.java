2f2ceae8e1cba04d8d8160829ce74d01bd8d3a2c
thomasra <thomasraimana@hotmail.fr>
2015-02-16 22:09:20 +0100

    PanelTableauDeBord et PanelAffichageVol en fin de travaux.

--- src/main/java/fr/iessa/vue/PanelAffichageVol.java
+++ src/main/java/fr/iessa/vue/PanelAffichageVol.java
@@ -1,10 +1,21 @@
 
 package fr.iessa.vue;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+
 import javax.swing.BorderFactory;
+import javax.swing.Box;
 import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JCheckBox;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+
+import fr.iessa.controleur.ModeleEvent;
 import fr.iessa.metier.trafic.Vol;
 import fr.iessa.metier.type.TypeVol;
 
@@ -13,39 +24,73 @@ import fr.iessa.metier.type.TypeVol;
  * @version 1.0 
  */
 
-public class PanelAffichageVol extends JPanel {
+public class PanelAffichageVol extends JPanel implements PropertyChangeListener {
 	/** Attributs */
-	private final JLabel _titre = new JLabel("PARAMATRES D'UN VOL :");
-	private final JLabel _parDefaut = new JLabel("Aucun vol selectionne");
     private JLabel _jlIdVol, _jlCategorieVol, _jlTypeHeureVol;
+    private final JCheckBox _checkBox = new JCheckBox("Texte");
+    private final JButton _boutonShow = new JButton("Show");
+    private final JButton _boutonRemove = new JButton("Remove");
     
     /** Constructeur */
-    public PanelAffichageVol() {
+    public PanelAffichageVol(Vol v) {
 		
     	super();
-    	setBorder(BorderFactory.createTitledBorder("PARAMATRES D'UN VOL :"));
+    	setBorder(BorderFactory.createTitledBorder(" Paramètres : "));
     	setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
-    	//_titre.setFont(new Font("Sans", Font.BOLD, 10));
-    	//add(_titre);
-		add(_parDefaut);
+    	add(Box.createHorizontalGlue()); //Pour que les éléments prennent toute la largeur du panel
 		
-	}
-	
-    public void SetVol(Vol v) {
-    	
-    	_jlIdVol = new JLabel(v.getId());
-    	_jlCategorieVol = new JLabel( (v.getCategorie()).getAbreviation() );
+		_jlIdVol = new JLabel("Identifiant : " + v.getId());
+    	_jlCategorieVol = new JLabel("Catégorie : " + (v.getCategorie()).getAbreviation() );
     	if (v.getTypeVol() == TypeVol.ARR) {
-    		_jlTypeHeureVol = new JLabel("Arrivée : " + v.getPremierInstant());
+    		_jlTypeHeureVol = new JLabel("Heure d'arrivée : " + v.getPremierInstant());
 	    } else {
-	    	_jlTypeHeureVol = new JLabel("Départ : " + v.getPremierInstant());
+	    	_jlTypeHeureVol = new JLabel("Heure de départ : " + v.getPremierInstant());
 	    }
     	
-    	remove(_parDefaut);
+    	_checkBox.addActionListener(new ActionCheck());
+    	_boutonShow.addActionListener(new ActionShow());
+    	_boutonRemove.addActionListener(new ActionRemove());
+    	
     	add(_jlIdVol);
     	add(_jlCategorieVol);
     	add(_jlTypeHeureVol);
-    	
+    	add(_checkBox);
+    	add(_boutonShow);
+    	add(_boutonRemove);
+		
+	}
+	
+    /** Listeners */
+    class ActionCheck implements ActionListener {
+    	public void actionPerformed(ActionEvent arg0) {
+    		
+    	}
     }
+    
+    class ActionShow implements ActionListener {
+    	public void actionPerformed(ActionEvent arg0) {
+    		
+    	}
+    }
+    
+    class ActionRemove implements ActionListener {
+    	public void actionPerformed(ActionEvent arg0) {
+    		// Pas sure
+    		removeAll();
+    	}
+    }
+    
+    /** Actions à realiser après une demande de mise à jour par le contrôleur */
+    public void propertyChange(PropertyChangeEvent evt) {
+    	
+		switch (ModeleEvent.valueOf(evt.getPropertyName())) {
+			case UPDATE_INSTANT:
+				break;
+
+			default:
+				break;
+		}
+		
+	}
 
 }
